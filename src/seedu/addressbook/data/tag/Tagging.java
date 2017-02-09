e8286c6af495196aff92d2528e9f63c24110d315
Ying <ying1996@gmail.com>
2017-02-09 17:51:10 +0800

    Add association class Tagging

---
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -0,0 +1,45 @@
+package seedu.addressbook.data.tag;
+
+import java.util.ArrayList;
+
+import seedu.addressbook.data.person.Person;
+
+public class Tagging {
+	public static enum TaggingOperation{
+		ADD, DELETE
+	}
+	
+	public static final String ADD_OPERATION = "+";
+	public static final String DELETE_OPERATION = "-";
+	
+	public static ArrayList<Tagging> taggingList = new ArrayList<Tagging>();
+	
+	private Person person;
+	private Tag tag;
+	private TaggingOperation type;
+	
+	public Tagging(Person person, Tag tag, TaggingOperation type) {
+		this.person = person;
+		this.tag = tag;
+		this.type = type;
+	}
+	
+	public static void addTag(Person person, Tag tag) {
+		taggingList.add(new Tagging(person, tag, TaggingOperation.ADD));
+	}
+	
+	public static void deleteTag(Person person, Tag tag) {
+		taggingList.add(new Tagging(person, tag, TaggingOperation.DELETE));
+	}
+	
+	public String getOperation() {
+		return type == TaggingOperation.ADD ? "+" : "-";
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return getOperation() + " " + person.getName() + " [" + tag.toString() + "]";
+	}
+}
